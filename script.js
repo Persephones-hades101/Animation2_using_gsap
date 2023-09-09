37049b3220d48990bb519dee6e7d52c9880d6376
sudh4800@gmail.com <sudh4800@gmail.com>
2023-09-10 03:03:30 +0530

    Commit 1

---
+++ script.js
@@ -0,0 +1,50 @@
+var tl =gsap.timeline()
+
+tl.from("#nav",{
+  opacity:0,
+  delay:1,
+  duration:0.3,
+})
+tl.from("#nav h1,#nav h3",{
+  y:-80,
+  opacity:0,
+  duration:0.5,
+  stagger:0.3,
+})
+tl.from("#left1",{
+  opacity:0,
+  duration:0.3,
+})
+tl.from("#left1 h1",{
+  x:-500,
+  opacity:0,
+  duartion:0.5,
+  stagger:0.3,
+})
+tl.from("#left2 h1",
+{
+  x:-500,
+  opacity:0,
+  duartion:0.5,
+  stagger:0.3,
+})
+
+tl.from("#right img ",{
+  scale:0.5,
+  opacity:0,
+  duration:0.5,
+})
+
+tl.from("#page2 .box",
+{
+  scale:0,
+  opacity:0,
+  duration:1,
+  stagger:0.3,
+  scrollTrigger:{
+    trigger:"#page2 .box",
+    scroller:"body"
+
+  }
+
+})
